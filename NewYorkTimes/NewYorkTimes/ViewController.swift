55c42f0078210ac0f52050167c8ac1344f1b95db
Alena Drobko <alena.drobko@onix-systems.com>
2023-06-08 20:31:02 +0200

    init workspace

---
+++ NewYorkTimes/NewYorkTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewYorkTimes
+//
+//  Created by Альона Дробко on 08.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
